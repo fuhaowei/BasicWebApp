4f44a57158c2374a8eefdf6c6b4ee3224952e5a1
haowei <haoweif@andrew.cmu.edu>
2022-09-26 00:29:57 -0400

    change made

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,7 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>Welcome!</h1>" +
+                "<h1>Welcome! I made a change</h1>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
